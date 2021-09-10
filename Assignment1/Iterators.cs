5d3f50a8aac6c60ea400e4741af71fd724a41fe8
T4NKG0D <maoe@itu.dk>
2021-09-10 12:59:35 +0200

    Added the files from the teacher

---
+++ Assignment1/Iterators.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace Assignment1
+{
+    public static class Iterators
+    {
+        public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
+        {
+            var T 
+    
+        }
+
+        public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static main(string[] args){
+            
+        }
+    }
+}
